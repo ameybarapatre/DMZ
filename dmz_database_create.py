286bd595ddae1f081a82b123034534321843b645
ameybarapatre <ameybarapatre@gmail.com>
2016-11-01 19:15:07 +0530

    Database creation

---
+++ dmz_database_create.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class services_dmz(Base):
+    __tablename__ = 'services_dmz'
+    service_name = Column(String(250))
+    service_port = Column(Integer,nullable=False)
+    service_protocol = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('users_dmz.user_id'))
+    service_id =Column(Integer,primary_key=True)
+
+class users_dmz(Base):
+    __tablename__ = 'users_dmz'
+    user_grp = Column(Integer ,nullable=True)
+    user_pass = Column(Integer , nullable=False)
+    user_name = Column(String(250), nullable=False)
+    user_id = Column(Integer , nullable=False , primary_key=True)
+    services  = relationship(services_dmz)
+
+
+engine = create_engine('sqlite:///dmz_database_version-1.0.db')
+
+
+Base.metadata.create_all(engine)
