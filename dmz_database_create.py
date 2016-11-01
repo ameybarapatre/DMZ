00590f0f76f557bf8aecd1413588ae320d2fb838
ameybarapatre <ameybarapatre@gmail.com>
2016-11-01 20:12:04 +0530

    database update

--- dmz_database_create.py
+++ dmz_database_create.py
@@ -9,11 +9,12 @@ Base = declarative_base()
 
 class services_dmz(Base):
     __tablename__ = 'services_dmz'
+    service_id = Column(Integer, primary_key=True)
     service_name = Column(String(250))
     service_port = Column(Integer,nullable=False)
     service_protocol = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('users_dmz.user_id'))
-    service_id =Column(Integer,primary_key=True)
+    users=relationship("users_dmz",back_populates="services")
 
 class users_dmz(Base):
     __tablename__ = 'users_dmz'
@@ -21,7 +22,7 @@ class users_dmz(Base):
     user_pass = Column(Integer , nullable=False)
     user_name = Column(String(250), nullable=False)
     user_id = Column(Integer , nullable=False , primary_key=True)
-    services  = relationship(services_dmz)
+    services  = relationship("services_dmz ",back_populates="users" )
 
 
 engine = create_engine('sqlite:///dmz_database_version-1.0.db')
