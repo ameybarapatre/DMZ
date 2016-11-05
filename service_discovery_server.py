7c6c49a8b0d82f5aeeaf16a9a663be9cd80a3ea5
ameybarapatre <ameybarapatre@gmail.com>
2016-11-06 03:44:46 +0530

    redis added

--- service_discovery_server.py
+++ service_discovery_server.py
@@ -1,6 +1,8 @@
 import socket
 import struct
 import sys
+import json
+from dmz_database_query import dmz_query
 
 multicast_group = '224.3.29.71'
 server_address = ('', 10000)
@@ -16,9 +18,18 @@ sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 while True:
     print( sys.stderr, '\nwaiting to receive message')
     data, address = sock.recvfrom(1024)
+    user=json.loads(data.decode('utf-8').replace("'", '"'))
 
     print( sys.stderr, 'received %s bytes from %s' % (len(data), address))
-    print (sys.stderr, data)
+    print (sys.stderr,user )
 
-    print (sys.stderr, 'sending acknowledgement to', address)
-    sock.sendto(b'ack', address)
+    validation="Invalid Credentials"
+    validity=dmz_query.view_user(user['username'],user['password'])
+    print(validity)
+    if validity!=None:
+        validation="Logged In"
+        print(sys.stderr, 'sending acknowledgement to', address)
+        sock.sendto(bytes(validation,'utf-8'), address)
+    else:
+        print (sys.stderr, 'sending acknowledgement to', address)
+        sock.sendto(bytes(validation, 'utf-8'), address)
