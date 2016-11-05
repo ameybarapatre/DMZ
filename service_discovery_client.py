9f77ed745b91f886c19aea51176b786a26174944
ameybarapatre <ameybarapatre@gmail.com>
2016-11-06 03:44:37 +0530

    redis added

--- service_discovery_client.py
+++ service_discovery_client.py
@@ -2,31 +2,35 @@ import socket
 import struct
 import sys
 
-message = b'very important data'
-multicast_group = ('224.3.29.71', 10000)
+def service_discovery(user={"username":"amey","password":"amy"}):
+    message = bytes(str(user), 'utf-8')
+    multicast_group = ('224.3.29.71', 10000)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(1)
+    ttl = struct.pack('b', 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-ttl = struct.pack('b', 1)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
+    try:
 
-try:
+        print (sys.stderr, 'sending "%s"' % message)
+        sent = sock.sendto(message, multicast_group)
 
-    print (sys.stderr, 'sending "%s"' % message)
-    sent = sock.sendto(message, multicast_group)
+        while True:
+            print (sys.stderr, 'waiting to receive')
+            try:
+                data, server = sock.recvfrom(20)
+            except socket.timeout:
+                print (sys.stderr, 'timed out, no more responses')
+                break
+            else:
+                print (sys.stderr, 'received "%s" from %s' % (data, server))
+                break
 
-    while True:
-        print (sys.stderr, 'waiting to receive')
-        try:
-            data, server = sock.recvfrom(16)
-        except socket.timeout:
-            print (sys.stderr, 'timed out, no more responses')
-            break
-        else:
-            print (sys.stderr, 'received "%s" from %s' % (data, server))
-            break
+    finally:
+        print (sys.stderr, 'closing socket')
+        sock.close()
 
-finally:
-    print (sys.stderr, 'closing socket')
-    sock.close()
+if __name__=='__main__':
+    service_discovery()
